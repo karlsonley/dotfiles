39314f2388ef87794ab9876bdfe7d99a56bb370d
Karl Martin Sonley <karlsonley@users.noreply.github.com>
2025-03-08 22:33:59 +0100

    docs: add author credit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,3 @@
+-- This setup is heavily inspired/bootstrapped from ttps://github.com/VVoruganti/dotfiles/blob/master/neovim/lua/marshmalon
+-- All credit to them and their tutorial on https://vineeth.io/posts/neovim-setup
 require("root")
