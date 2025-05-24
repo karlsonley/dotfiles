ae0deb8b74a9c39777af12ac1c1b239e7923619e
Karl Martin Sonley <6653124+karlsonley@users.noreply.github.com>
2025-05-24 17:40:18 +0200

    feat: add nvidia drivers

--- install/dnf.sh
+++ install/dnf.sh
@@ -3,5 +3,9 @@ sudo dnf upgrade -y
 
 sudo dnf install -y \
   neovim \
+  akmod-nvidia \
+  xorg-x11-drv-nvidia-cuda
 
-
+# Manually ensure akmod-nvidia is not removed by dnf autoremove
+# https://rpmfusion.org/Howto/NVIDIA#dnf_autoremove
+sudo dnf mark user akmod-nvidia
