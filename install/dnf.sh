436514af8a571cad5da376af5f33518d26d09e76
Karl Martin Sonley <6653124+karlsonley@users.noreply.github.com>
2025-05-24 17:54:13 +0200

    feat: add standard packages

--- install/dnf.sh
+++ install/dnf.sh
@@ -3,9 +3,26 @@ sudo dnf upgrade -y
 
 sudo dnf install -y \
   neovim \
+  gcc \
+  tmux \
+  git \
+  curl \
+  wget \
+  tar \
+  gzip \
   akmod-nvidia \
-  xorg-x11-drv-nvidia-cuda
+  xorg-x11-drv-nvidia-cuda \
+
+# Lazygit
+sudo dnf copr enable atim/lazygit -y
+sudo dnf install lazygit -y
+
 
 # Manually ensure akmod-nvidia is not removed by dnf autoremove
 # https://rpmfusion.org/Howto/NVIDIA#dnf_autoremove
 sudo dnf mark user akmod-nvidia
+
+# Cleanup
+sudo dnf -y autoremove
+sudo dnf -y clean all
+
