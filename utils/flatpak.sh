b1f84e8d7989dc7e8cf5b7126ac3a10e4ea67306
Karl Martin Sonley <6653124+karlsonley@users.noreply.github.com>
2025-05-25 10:55:21 +0200

    feat: add zen browser

--- utils/flatpak.sh
+++ utils/flatpak.sh
@@ -7,6 +7,7 @@ flatpak install -y flathub \
   com.valvesoftware.Steam \
   com.heroicgameslauncher.hgl \
   com.spotify.Client \
+  app.zen_browser.zen \
 
 # Cleanup
 sudo flatpak uninstall --unused
