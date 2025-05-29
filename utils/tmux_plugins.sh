a24463d1317eafb1d970e63cb0cf5037b9984fad
Karl Martin Sonley <6653124+karlsonley@users.noreply.github.com>
2025-05-29 15:57:45 +0200

    feat: install catpuccin theme

--- utils/tmux_plugins.sh
+++ utils/tmux_plugins.sh
@@ -1,8 +1,19 @@
+# Install TPM
 clone_dir=$HOME/.tmux/plugins/tpm
 
 if [ ! -d $clone_dir ]
 then
   git clone https://github.com/tmux-plugins/tpm $clone_dir
+else
+  (cd $clone_dir && git pull)
+fi
+
+# Install Catppuccin TMUX
+clone_dir=$HOME/.config/tmux/plugins/catppuccin/tmux
+
+if [ ! -d $clone_dir ]
+then
+  git clone https://github.com/catppuccin/tmux.git $clone_dir
 else
   (cd $clone_dir && git pull)
 fi
