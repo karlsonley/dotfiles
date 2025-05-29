d77c2217993f8c190e61615de2fd4334d4de7861
Karl Martin Sonley <6653124+karlsonley@users.noreply.github.com>
2025-05-29 15:58:31 +0200

    feat: install catppuccin theme for terminal

--- install_fedora.sh
+++ install_fedora.sh
@@ -4,7 +4,11 @@
 # Lazy creates an empty configfile on download
 rm -rf ~/.config/lazygit
 
+# Install Catppuccin in Gnome Terminal
+curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 -
+
 stow --dotfiles --target="$HOME" \
   neovim \
   git \
   lazygit \
+  tmux \ 
