43f9b950eb7ae307fbca3214ddf9e961cf7eb408
Karl Martin Sonley <6653124+karlsonley@users.noreply.github.com>
2025-05-29 16:54:36 +0200

    feat: add tmux plugins to fedora install

--- install_fedora.sh
+++ install_fedora.sh
@@ -1,14 +1,12 @@
 ./utils/dnf.sh
 ./utils/flatpak.sh
+./utils/tmux_plugins.sh
 
 # Lazy creates an empty configfile on download
 rm -rf ~/.config/lazygit
-
-# Install Catppuccin in Gnome Terminal
-curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 -
 
 stow --dotfiles --target="$HOME" \
   neovim \
   git \
   lazygit \
-  tmux \ 
+  tmux
