7d571014d88356e75ad7eb36feee6277d4199789
Karl Martin Sonley <6653124+karlsonley@users.noreply.github.com>
2025-06-20 23:52:55 +0200

    feat: add mini neovim plugins

---
+++ neovim/dot-config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    'echasnovski/mini.move',
+    version = false,
+    config = function()
+      require('mini.move').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.pairs',
+    version = false,
+    config = function()
+      require('mini.pairs').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    config = function()
+      require('mini.surround').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.cursorword',
+    version = false,
+    config = function()
+      require('mini.cursorword').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.indentscope',
+    version = false,
+    config = function()
+      require('mini.indentscope').setup()
+    end,
+  },
+  {
+    'echasnovski/mini.trailspace',
+    version = false,
+    config = function()
+      require('mini.trailspace').setup()
+    end,
+  },
+}
